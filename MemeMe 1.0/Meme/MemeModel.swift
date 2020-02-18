ec4e55de70720e8528b7fb484c58c2d1358b5381
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-18 16:41:16 +0000

    add MemeModel struct

---
+++ MemeMe 1.0/Meme/MemeModel.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  Meme
+//
+//  Created by Anastasia Petrova on 18/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+
+struct MemeModel {
+    var topTetx: String
+    var bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
