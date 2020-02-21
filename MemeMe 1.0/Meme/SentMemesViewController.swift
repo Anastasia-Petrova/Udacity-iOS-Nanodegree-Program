ab651fc4ec7725bc8f450c2bc616cf881318b20a
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-21 17:52:47 +0000

    add tableView and setUpTableView()

--- MemeMe 1.0/Meme/SentMemesViewController.swift
+++ MemeMe 1.0/Meme/SentMemesViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class SentMemesViewController: UIViewController {
+final class SentMemesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.title = "Sent Memes"
         setUpNavigationBar()
+        setUpTableView()
+        setUpTabBar()
     }
     
     private func setUpNavigationBar() {
@@ -22,8 +24,38 @@ class SentMemesViewController: UIViewController {
         navigationItem.rightBarButtonItem = addItem
     }
     
+    private func setUpTabBar() {
+        let tabBar = UITabBar()
+        tabBar.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tabBar)
+        let gridBarItem = UITabBarItem()
+        let collectionBarItem = UITabBarItem()
+        NSLayoutConstraint.activate([
+            tabBar.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            tabBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tabBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+        
+        gridBarItem.image = UIImage(systemName: "rectangle.split.3x3")
+        collectionBarItem.image = UIImage(systemName: "list.dash")
+        tabBar.setItems([collectionBarItem, gridBarItem], animated: false)
+    }
+    
+    private func setUpTableView() {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
+    
     @objc func pushEditorViewController() {
         let vc = MemeEditorViewController()
         self.navigationController?.pushViewController(vc, animated: true)
     }
 }
+
