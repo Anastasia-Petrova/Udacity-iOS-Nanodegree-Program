581698d062e9791dd289770ed9af30882f20bc07
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-24 12:54:55 +0000

    add CollectionViewDataSource and CollectionViewCell

---
+++ MemeMe 1.0/Meme/CollectionView/CollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewCell.swift
+//  Meme
+//
+//  Created by Anastasia Petrova on 24/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+
+final class CollectionViewCell: UICollectionViewCell {
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+    super.init(frame: frame)
+        self.contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        imageView.contentMode = .scaleAspectFill
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ MemeMe 1.0/Meme/CollectionView/CollectionViewDataSource.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewDataSource.swift
+//  Meme
+//
+//  Created by Anastasia Petrova on 24/02/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+
+final class CollectionViewDataSource: NSObject,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+
+}
