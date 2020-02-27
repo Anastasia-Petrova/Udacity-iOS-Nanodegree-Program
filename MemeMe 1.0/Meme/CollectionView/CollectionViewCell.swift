e5790d235e0ca7f9db9e533a749162d801336f8f
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-27 21:19:31 +0000

    add delete circle image to collection view cell

--- MemeMe 1.0/Meme/CollectionView/CollectionViewCell.swift
+++ MemeMe 1.0/Meme/CollectionView/CollectionViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 final class CollectionViewCell: UICollectionViewCell {
     let memeImageView = UIImageView()
+    //TODO: make this a button
+    let deleteImageView = UIImageView()
     
     override init(frame: CGRect) {
     super.init(frame: frame)
@@ -23,10 +25,29 @@ final class CollectionViewCell: UICollectionViewCell {
         ])
         memeImageView.contentMode = .scaleAspectFill
         memeImageView.clipsToBounds = true
+        setUpDeleteImageView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setUpDeleteImageView() {
+        deleteImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(deleteImageView)
+        NSLayoutConstraint.activate([
+            deleteImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -4),
+            deleteImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
+            deleteImageView.widthAnchor.constraint(equalToConstant: 40),
+            deleteImageView.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        deleteImageView.contentMode = .scaleAspectFit
+        deleteImageView.image = UIImage(systemName: "minus.circle")
+        deleteImageView.tintColor = .red
+        deleteImageView.isHidden = true
+    }
+    
+//    func buttonSelector() {
+//        didTapDeleteButton()
+//    }
 }
