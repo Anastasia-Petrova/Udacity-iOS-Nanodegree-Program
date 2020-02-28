5a52e43feb51e254e070be2839258b5a919cc9b0
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-02-28 00:29:26 +0000

    clean up MemesStorage

--- MemeMe 1.0/Meme/MemesStorage.swift
+++ MemeMe 1.0/Meme/MemesStorage.swift
@@ -9,49 +9,21 @@
 import UIKit
 
 enum MemesStorage {
-    public static func decodeData() -> [MemeModel] {
-        guard let encodedData = UserDefaults.standard.data(forKey: "memes") else {
-            return []
-        }
-        return try! JSONDecoder().decode([MemeModel].self, from: encodedData)
-    }
+    private static let defaults = UserDefaults.standard
     
-    public static func encodeData(memes: [MemeModel]) {
-        let encodedData = try! JSONEncoder().encode(memes)
+    public static func save(memes: [MemeModel]) throws {
+        let encodedData = try JSONEncoder().encode(memes)
         UserDefaults.standard.set(encodedData, forKey: "memes")
     }
     
-    public static func getAllIMages(memes: [MemeModel]) -> [(text: String, image: UIImage)] {
-        let sortedMemes = memes.sorted(by: { $1.date > $0.date })
-        var textAndImagesArray: [(String, UIImage)] = []
-        sortedMemes.forEach { meme in
-            let text = meme.topTetx + "..." + meme.bottomText
-            let directoryURL = ImageStorage.memesDirectory.appendingPathComponent(meme.id.uuidString)
-            if let image = UIImage(contentsOfFile: directoryURL.path) {
-                textAndImagesArray.append((text, image))
-            }
+    public static func loadMemes() throws -> [MemeModel] {
+        guard let encodedData = UserDefaults.standard.data(forKey: "memes") else {
+            return []
         }
-        return textAndImagesArray
+        return try JSONDecoder().decode([MemeModel].self, from: encodedData)
     }
     
-    public func getAllTextsAndImages(memes: [MemeModel]) -> [(text: String, image: UIImage)] {
-        let sortedMemes = memes.sorted(by: { $1.date > $0.date })
-        var textAndImagesArray: [(String, UIImage)] = []
-        sortedMemes.forEach { meme in
-            let text = meme.topTetx + "..." + meme.bottomText
-            let directoryURL = ImageStorage.memesDirectory.appendingPathComponent(meme.id.uuidString)
-            if let image = UIImage(contentsOfFile: directoryURL.path) {
-                textAndImagesArray.append((text, image))
-            }
-        }
-        return textAndImagesArray
-    }
-    
-    public func deleteMeme(memes: [MemeModel], indexPath: IndexPath) -> [MemeModel]{
-        let item = memes[indexPath.row]
-        let id = item.id
-        let directoryURL = ImageStorage.memesDirectory.appendingPathComponent(id.uuidString)
-        try? ImageStorage.deleteImage(url: directoryURL)
+    public static func delete(memes: [MemeModel], indexPath: IndexPath) -> [MemeModel] {
         var array = memes
         array.remove(at: indexPath.row)
         let encodedData = try! JSONEncoder().encode(memes)
