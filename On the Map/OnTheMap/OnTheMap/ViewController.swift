cc15d3932150e2f962fede14d2fae5b2e4d63065
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-03-12 11:28:48 +0000

    create new project and add assets

---
+++ On the Map/OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Anastasia Petrova on 12/03/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
