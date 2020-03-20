b09a0516a174f64c61c2256458351493a9dc92c0
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-03-20 20:41:21 +0000

    add MapViewController

---
+++ On the Map/OnTheMap/OnTheMap/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Anastasia Petrova on 20/03/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    let mapView = MKMapView(frame: .zero)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        mapView.region = 
+
+    }
+
+}
