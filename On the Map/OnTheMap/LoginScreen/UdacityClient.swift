18fd945bbc66a3cce2b37f7ae1c9e4b3c2183e79
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-03-22 15:53:28 +0000

    add getUserInfo() and logout() methods

--- On the Map/OnTheMap/LoginScreen/UdacityClient.swift
+++ On the Map/OnTheMap/LoginScreen/UdacityClient.swift
@@ -44,4 +44,41 @@ class UdacityClient {
         }
         task.resume()
     }
+    
+    class func getUserInfo() {
+        let request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/users/3903878747")!)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+          if error != nil { // Handle error...
+              return
+          }
+          let range = 5..<data!.count
+          let newData = data?.subdata(in: range) /* subset response data! */
+          print(String(data: newData!, encoding: .utf8)!)
+        }
+        task.resume()
+    }
+    
+    class func logout() {
+        var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
+        request.httpMethod = "DELETE"
+        var xsrfCookie: HTTPCookie? = nil
+        let sharedCookieStorage = HTTPCookieStorage.shared
+        for cookie in sharedCookieStorage.cookies! {
+          if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+          request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+          if error != nil { // Handle error…
+              return
+          }
+          let range = 5..<data!.count
+          let newData = data?.subdata(in: range) /* subset response data! */
+          print(String(data: newData!, encoding: .utf8)!)
+        }
+        task.resume()
+    }
 }
