15c20124c8321f2c6b431c9f1364cebe46f6e344
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-03-23 20:06:38 +0000

    add StudentsLocations

---
+++ On the Map/OnTheMap/Students .swift
@@ -0,0 +1,35 @@
+//
+//  StudentsLocations .swift
+//  OnTheMap
+//
+//  Created by Anastasia Petrova on 23/03/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import Foundation
+
+struct StudentsLocations: Codable {
+    let creationDate: String
+    let firstName: String
+    let lastName: String
+    let latitude: Float
+    let longlatitude: Float
+    let location: String
+    let link: String
+    let objectID: String
+    let uniqueKey: String
+    let updateDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case creationDate = "createdAt"
+        case firstName = "firstName"
+        case lastName = "lastName"
+        case latitude = "latitude"
+        case longlatitude = "longitude"
+        case location = "mapString"
+        case link = "mediaURL"
+        case objectID = "objectId"
+        case uniqueKey = "uniqueKey"
+        case updateDate = "updatedAt"
+    }
+}
