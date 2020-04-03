f55014414c25a0155eecb469c0c1d0cdcbec8e3b
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-04-03 20:34:39 +0100

    set root VC

--- Virtual Tourist/VirtualTourist/VirtualTourist/SceneDelegate.swift
+++ Virtual Tourist/VirtualTourist/VirtualTourist/SceneDelegate.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let rootVC = TravelMapViewController()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
     }
 
 }
