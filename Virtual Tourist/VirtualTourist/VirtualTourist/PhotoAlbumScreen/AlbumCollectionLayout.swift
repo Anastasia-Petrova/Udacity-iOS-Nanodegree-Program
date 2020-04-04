0d179be473ee68d87268ccc52e044c0f7d7d96e2
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-04-04 12:18:31 +0100

    add AlbumCollectionLayout

---
+++ Virtual Tourist/VirtualTourist/VirtualTourist/PhotoAlbumScreen/AlbumCollectionLayout.swift
@@ -0,0 +1,36 @@
+//
+//  AlbumCollectionLayout.swift
+//  VirtualTourist
+//
+//  Created by Anastasia Petrova on 04/04/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCollectionLayout: UICollectionViewFlowLayout {
+    static let spacing: CGFloat = 4.0
+    
+    override init() {
+        super.init()
+        self.itemSize = .zero
+        self.sectionInset = UIEdgeInsets(
+            top: AlbumCollectionLayout.spacing,
+            left: 0,
+            bottom: 0,
+            right: 0
+        )
+        self.minimumInteritemSpacing = Self.spacing
+        self.minimumLineSpacing = Self.spacing
+        self.scrollDirection = .vertical
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setWidth(width: CGFloat) {
+        let itemWidth = (width - Self.spacing * 2)/3.0
+        self.itemSize = CGSize(width: itemWidth, height: itemWidth)
+    }
+}
