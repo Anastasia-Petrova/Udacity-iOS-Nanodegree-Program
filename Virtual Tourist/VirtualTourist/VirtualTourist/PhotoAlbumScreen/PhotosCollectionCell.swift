fef08e99b8495a08ef1e09a69101ccb5ac4fae28
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-04-04 12:17:50 +0100

    add PhotosCollectionCell

---
+++ Virtual Tourist/VirtualTourist/VirtualTourist/PhotoAlbumScreen/PhotosCollectionCell.swift
@@ -0,0 +1,34 @@
+//
+//  PhotosCollectionCell.swift
+//  VirtualTourist
+//
+//  Created by Anastasia Petrova on 04/04/2020.
+//  Copyright © 2020 Anastasia Petrova. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionCell: UICollectionViewCell {
+    static let identifier = "PhotosCollectionCell"
+    
+    let photoImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+    super.init(frame: frame)
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(photoImageView)
+        NSLayoutConstraint.activate([
+            photoImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            photoImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            photoImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            photoImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        photoImageView.contentMode = .scaleAspectFill
+        photoImageView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
