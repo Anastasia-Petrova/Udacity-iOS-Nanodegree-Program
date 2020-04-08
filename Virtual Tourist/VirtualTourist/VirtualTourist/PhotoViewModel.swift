0bb85aa1666d7705ae442b0ca6f7673b71239c53
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-04-08 17:57:27 +0100

    add fileID to PhotoViewModel

--- Virtual Tourist/VirtualTourist/VirtualTourist/PhotoViewModel.swift
+++ Virtual Tourist/VirtualTourist/VirtualTourist/PhotoViewModel.swift
@@ -10,8 +10,10 @@ import EasyCoreData
 
 struct PhotoViewModel: CoreDataMappable {
     let url: URL?
+    let fileID: UUID?
     
     init(model: Photo) {
+        fileID = model.fileID
         url = model.url
     }
 }
