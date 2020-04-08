5cf7d3a8a2201b0233f4594896cca23cdfd0d753
Anastasia Petrova <anastasia.iosdev@gmail.com>
2020-04-08 22:06:07 +0100

    clean up code

--- Virtual Tourist/VirtualTourist/VirtualTourist/PhotoAlbumScreen/PhotoAlbumViewController.swift
+++ Virtual Tourist/VirtualTourist/VirtualTourist/PhotoAlbumScreen/PhotoAlbumViewController.swift
@@ -139,7 +139,6 @@ extension PhotoAlbumViewController: UICollectionViewDelegate {
     }
 }
 
-
 extension PhotoAlbumViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 150)
